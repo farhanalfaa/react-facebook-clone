e42abec1f0a103a78aaf295736113f358a0d4c05
Farhan Alfariqi <falfariqi18@gmail.com>
2020-10-19 11:20:31 +0700

    Adding Google Auth

--- src/App.js
+++ src/App.js
@@ -4,16 +4,25 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import Feed from "./Feed";
 import Widgets from "./Widgets";
+import Login from "./Login";
 
 function App() {
+  const user = null;
+
   return (
     <div className="app">
-      <Header />
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        <Widgets />
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <>
+          <Header />
+          <div className="app__body">
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </>
+      )}
     </div>
   );
 }
